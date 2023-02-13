5a943c1e704f8ef482f26be76a3805bbb44c1ad3
chinmay1819 <chinmaydeshpande34@gmail.com>
2023-02-13 14:45:05 +0530

    final commit

---
+++ models/form.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+
+
+const formSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+
+    questions: {
+      type:[
+        {
+          questionContent:{type:String},
+          questionNumber:{type:Number}
+        }
+      ]
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+  { typeKey: "$type" }
+);
+
+module.exports = mongoose.model("Form", formSchema);
---
+++ routes/formRoutes.js
@@ -0,0 +1,24 @@
+const express=require('express');
+
+const auth=require('../middlewares/auth');
+const authforadmin=require('../middlewares/authforadmin')
+
+
+const {getForms,createForm,getAllForms,deleteForm,updateForm}=require('../controllers/formController')
+const formRouter=express.Router();
+
+
+formRouter.get('/',auth,getForms)
+
+formRouter.post('/',auth,createForm)
+
+
+formRouter.delete("/:id",auth,deleteForm);
+
+formRouter.put("/:id",auth,updateForm);
+
+formRouter.get('/allforms',auth,authforadmin,getAllForms);
+
+
+module.exports=formRouter
+
